6150d398145340c92d8a73f0de7c7c06b7cadee5
fogiky <fogiky@gmail.com>
2024-04-12 04:49:14 +0300

    .

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
-version = '0.0.1'
+from setuptools import setup, find_packages
 
+VERSION = '0.0.1'
+DESCRIPTION = 'LOLZTEAM api package'
 
+# Setting up
+setup(
+    name="LZT_api",
+    version=VERSION,
+    author="fogiky",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python', 'LOLZTEAM'],
+)
